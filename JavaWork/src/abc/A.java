b1eda85a98a13f05b2ecd29c72e758c4c08ecbae
testingworldglobal <testingworldglobal@gmail.com>
2017-06-04 13:19:05 +0530

    JIRA_001 : Created Automation Project

---
+++ JavaWork/src/abc/A.java
@@ -0,0 +1,17 @@
+package abc;
+
+public class A {
+
+	protected void abc()
+	{
+	 System.out.println("Hello");	
+	}
+	
+	public void mno()
+	{
+		this.abc();
+	}
+	
+}
+
+
---
+++ JavaWork/src/testing/B.java
@@ -0,0 +1,24 @@
+package testing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import abc.A;
+
+public class B  {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		byte b=10;   // 1 byte  
+		short s;  // 2
+		int i=100;    //4
+	    long l;   //8
+		
+			
+			s=(short)i;
+			
+	}
+}
